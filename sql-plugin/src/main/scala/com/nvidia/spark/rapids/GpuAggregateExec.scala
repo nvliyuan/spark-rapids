7a26525eef6d2b235c10850d9cf628fc36ca069d
Renjie Liu <liurenjie2008@gmail.com>
2024-07-02 09:40:29 +0800

    Introduce LORE framework. (#11084)
    * Introduce lore id
    
    * Introduce lore id
    
    * Fix type
    
    * Fix type
    
    * Conf
    
    * style
    
    * part
    
    * Dump
    
    * Introduce lore framework
    
    * Add tests.
    
    * Rename test case
    
    Signed-off-by: liurenjie1024 <liurenjie2008@gmail.com>
    
    * Fix AQE test
    
    * Fix style
    
    * Use args to display lore info.
    
    * Fix build break
    
    * Fix path in loreinfo
    
    * Remove path
    
    * Fix comments
    
    * Update configs
    
    * Fix comments
    
    * Fix config
    
    ---------
    
    Signed-off-by: liurenjie1024 <liurenjie2008@gmail.com>

--- sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuAggregateExec.scala
+++ sql-plugin/src/main/scala/com/nvidia/spark/rapids/GpuAggregateExec.scala
@@ -2000,6 +2000,7 @@ case class GpuHashAggregateExec(
        |${ExplainUtils.generateFieldString("Functions", aggregateExpressions)}
        |${ExplainUtils.generateFieldString("Aggregate Attributes", aggregateAttributes)}
        |${ExplainUtils.generateFieldString("Results", resultExpressions)}
+       |Lore: ${loreArgs.mkString(", ")}
        |""".stripMargin
   }
 
@@ -2130,10 +2131,12 @@ case class GpuHashAggregateExec(
       truncatedString(allAggregateExpressions, "[", ", ", "]", maxFields)
     val outputString = truncatedString(output, "[", ", ", "]", maxFields)
     if (verbose) {
-      s"GpuHashAggregate(keys=$keyString, functions=$functionString, output=$outputString)"
+      s"$nodeName (keys=$keyString, functions=$functionString, output=$outputString) " +
+        s"""${loreArgs.mkString(", ")}"""
     } else {
-      s"GpuHashAggregate(keys=$keyString, functions=$functionString)," +
-          s" filters=${aggregateExpressions.map(_.filter)})"
+      s"$nodeName (keys=$keyString, functions=$functionString)," +
+          s" filters=${aggregateExpressions.map(_.filter)})" +
+          s""" ${loreArgs.mkString(", ")}"""
     }
   }
   //
